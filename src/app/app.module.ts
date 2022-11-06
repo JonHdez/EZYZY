da8651dd7c3089a1270c9ee844cd91e2d831d0b1
The-Running-Riot <101164427+The-Running-Riot@users.noreply.github.com>
2022-11-06 00:14:44 -0600

    LandingPage
    Modo Escritorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,16 +7,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageModule } from './landing-page/landing-page.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LandingPageModule,
     BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  exports:[
   ],
   providers: [],
   bootstrap: [AppComponent]
