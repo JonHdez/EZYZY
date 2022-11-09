7c416908cddec516e74c67837929211089087fa7
The-Running-Riot <101164427+The-Running-Riot@users.noreply.github.com>
2022-11-09 14:52:31 -0600

    matTabsModule

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,8 +1,8 @@
+import { MatTabsModule } from '@angular/material/tabs';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { HomeAdminComponent } from './home-admin/home-admin.component';
-import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 
 
@@ -15,14 +15,14 @@ import { MatButtonModule } from '@angular/material/button';
 
   imports: [
     CommonModule,
-    MatTabsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTabsModule
   ],
 
   exports: [
     CommonModule,
-    MatTabsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTabsModule
   ]
 })
 export class AdminModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,19 +7,20 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
-import {MatTabsModule} from '@angular/material/tabs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageModule } from './landing-page/landing-page.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './footer/footer.component';
 import { MatCardModule } from '@angular/material/card';
+import { HomeAdminComponent } from './admin/home-admin/home-admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    LoginClienteComponent
+    LoginClienteComponent,
+    HomeAdminComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +33,7 @@ import { MatCardModule } from '@angular/material/card';
     MatCardModule,
     MatButtonModule,
     HttpClientModule,
-    MatTabsModule
+    MatTabsModule,
   ],
   exports:[FooterComponent
   ],
