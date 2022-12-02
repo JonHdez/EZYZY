0e9e96344a0767e152403fe1b3d582304c79b0ae
JonHdez <Jonathan.DHernandezR@gmail.com>
2022-12-01 21:19:37 -0600

    merge con la main

---
+++ src/app/code-box/code-box.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CodeBoxComponent } from './code-box.component';
+
+
+
+@NgModule({
+  declarations: [
+    CodeBoxComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CodeBoxModule { }
