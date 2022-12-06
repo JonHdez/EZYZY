525f341f1442e35310b5152a7458ba2b9e7e4e7e
The-Running-Riot <101164427+The-Running-Riot@users.noreply.github.com>
2022-12-06 13:21:28 -0600

    registroCliente

--- src/app/cliente/registro-cliente/registro-cliente.component.ts
+++ src/app/cliente/registro-cliente/registro-cliente.component.ts
@@ -1,6 +1,8 @@
+import { ClientesService } from './../../services/clientes.service';
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {FormControl, FormBuilder, FormGroupDirective, NgForm, Validators, FormGroup} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import { Cliente } from 'src/app/interfaces/cliente.interface';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -17,18 +19,70 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./registro-cliente.component.scss']
 })
 export class RegistroClienteComponent implements OnInit {
+
+  public  registroForm!: FormGroup;
+  clienteRegistro!: Cliente;
+  hide = true;
+
   minPw = 8;
   maxN=20;
-  constructor() { }
+  constructor(private formBuilder:FormBuilder, private registrocliente:ClientesService) { }
+
 
   apellido= new FormControl('', [Validators.required, Validators.maxLength(this.maxN)]);
   nombre= new FormControl('', [Validators.required, Validators.maxLength(this.maxN)]);
-  password= new FormControl('', [Validators.required, Validators.minLength(this.minPw)]);
+  password= new FormControl('', [Validators.required, Validators.pattern(
+    /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+  )]);
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  
 
   matcher = new MyErrorStateMatcher();
 
+   
+
   ngOnInit(): void {
+    this.registroForm = this.formBuilder.group({
+      primerNombre: [''],
+      primerApellido : [''],
+      correo: [''],
+    })
+    console.log('entrada', this.registroForm.value)
   }
 
+  
+
+  registro(){
+
+    if ((this.password.hasError('required')||  this.password.hasError('pattern')|| this.apellido.hasError('required') ||
+    this.nombre.hasError('required') || this.emailFormControl.hasError('required') || 
+    this.emailFormControl.hasError('email') === true)&& (!this.apellido.hasError('required') ||
+    !this.emailFormControl.hasError('required')===false)){
+
+      console.log('password error',this.password.hasError('required'),
+      this.emailFormControl.hasError('required'))
+      alert('Rellene todos los campos correctamente')
+    } else {
+      this.registroForm = this.formBuilder.group({
+        primerNombre: [this.nombre.value],
+        primerApellido : [this.apellido.value],
+        correo: [this.emailFormControl.value],
+        contraseña:[this.password.value],
+        foto:[''],
+        status:['Activa']
+      })
+
+      this.clienteRegistro=({
+        nombre:this.nombre.value!,
+        Apellido:this.apellido.value!,
+        correo:this.emailFormControl.value!,
+        pasword:this.password.value!,
+        fotoUrl:'',
+        status:'Activa'
+      })
+      console.log('password error',this.password.hasError('pattern'))
+      console.log('as',this.clienteRegistro)
+    }
+
+  }
 }
