d00ddbf4ff4004c55732bcc2b8323104881a2f93
The-Running-Riot <101164427+The-Running-Riot@users.noreply.github.com>
2022-12-06 01:21:30 -0600

    login
    login cliente authenticacion

--- src/app/cliente/login-cliente/login-cliente.component.ts
+++ src/app/cliente/login-cliente/login-cliente.component.ts
@@ -2,8 +2,9 @@ import { ClientesService } from './../../services/clientes.service';
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from 'src/app/interfaces/cliente.interface';
 import {tap} from 'rxjs/operators';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import { Router } from '@angular/router';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -19,10 +20,14 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./login-cliente.component.scss']
 })
 export class LoginClienteComponent implements OnInit {
+
+  public loginForm!: FormGroup;
   minPw = 8;
   clientes!: Cliente[];
-  constructor(private clienteSvc:ClientesService) { }
+  constructor(private clienteSvc:ClientesService, private formBuilder: FormBuilder, private router:Router) { }
 
+  cliente:any=[];
+  indexArrayCliente:any=[];
   hide = true;
   password= new FormControl('', [Validators.required, Validators.minLength(this.minPw)]);
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
@@ -30,6 +35,12 @@ export class LoginClienteComponent implements OnInit {
   matcher = new MyErrorStateMatcher();
 
   ngOnInit(): void {
+        
+    this.loginForm= this.formBuilder.group({
+      correo:[''],
+      contraseña:['']
+    });
+    
     this.clienteSvc.getClientes().pipe(
       tap((clientes:Cliente[]) => {
         this.clientes = clientes
@@ -40,6 +51,34 @@ export class LoginClienteComponent implements OnInit {
     )
     .subscribe();
 
-  }
-}
 
+  }
+
+  login(){
+    this.clienteSvc.getClientes()
+    .subscribe(res=>{
+        const usuario = res.find((a:any)=>{
+          console.log('vairbale y contrasenia', a.correo, a.pasword)
+          console.log('vairbale y contrasenia log', this.loginForm.value.correo, this.loginForm.value.contraseña)
+          return a.correo === this.loginForm.value.correo && a.pasword === this.loginForm.value.contraseña
+        });
+        if(usuario){
+          alert("Logeado exitosamente");
+          this.loginForm.reset();
+          this.router.navigate(['home']);
+        }else{
+          
+          alert('Usuario no encontrado')
+        }
+        },
+        error=>{
+          console.log(error);
+      });
+    }
+  }
+
+
+// const usuario =
+// this.cliente.forEach((el: any, i: any) => {
+// this.indexArrayCliente.push(i);
+// return this.cliente[i].correo === this.loginForm.value.correo && this.cliente[i].pasword === this.loginForm.value.contraseña
