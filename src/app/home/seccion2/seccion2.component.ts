23d4db451ebefbdb2fd32404297dc147b2c9737e
The-Running-Riot <101164427+The-Running-Riot@users.noreply.github.com>
2022-12-08 23:19:02 -0600

    home plantillas
    xd

--- src/app/home/seccion2/seccion2.component.ts
+++ src/app/home/seccion2/seccion2.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
+import { Plantilla } from 'src/app/interfaces/plantilla.interface';
+import { PlantillasService } from 'src/app/services/plantilla.service';
 
 @Component({
   selector: 'app-seccion2',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Seccion2Component implements OnInit {
 
-  constructor() { }
+  private form1="";
+  plantillas!: Plantilla[];
+  codigoHtmlPlantilla:any;
+  constructor(
+    private plantillaSvc: PlantillasService,
+    private router: Router
+    ) {}
+
+  verPlantilla(id:any) {
+    console.log(id)
+    this.getPlantilla(id)
+    this.form1= id;
+    this.router.navigate(["/plantillas/"+this.form1], )
+  }
+
+  getPlantilla(id: string){
+      this.plantillaSvc.getPlantilla(id).subscribe({
+      next:(res)=>{
+        this.codigoHtmlPlantilla = res.codigohtml;
+        console.log(`codigo plantilla ${res.tituloDePlantilla}`,this.codigoHtmlPlantilla);
+      }
+    });
+  }
 
   ngOnInit(): void {
+    this.plantillaSvc
+    .getPlantillas()
+    .pipe(
+      tap((plantillas: Plantilla[]) => {
+        this.plantillas = plantillas;
+        console.log(this.plantillas);
+      })
+    )
+    .subscribe();
   }
 
 }
